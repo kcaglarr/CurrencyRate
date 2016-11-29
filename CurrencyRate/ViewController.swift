9b51d67632b8f1801029fd75850d1783157f9acf
KerimCaglar <kerim.caglarr@gmail.com>
2016-11-29 21:54:02 +0300

    Initial Commit

---
+++ CurrencyRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrencyRate
+//
+//  Created by Kerim Çağlar on 29/11/2016.
+//  Copyright © 2016 Kerim Çağlar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
