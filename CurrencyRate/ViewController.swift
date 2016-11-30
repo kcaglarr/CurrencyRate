0d9132f44295e5e3317f71fe99f33bca564c9a50
KerimCaglar <kerim.caglarr@gmail.com>
2016-11-30 23:32:35 +0300

    Currency app released

--- CurrencyRate/ViewController.swift
+++ CurrencyRate/ViewController.swift
@@ -10,16 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var sellingDollar: UILabel!
+    
+    @IBOutlet weak var buyingDollar: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let url = NSURL(string: "http://www.doviz.com/api/v1/currencies/all/latest")
+        let task = URLSession.shared.dataTask(with: url as! URL){(data,response,error) -> Void in
+            
+            if error != nil
+            {
+                print(error!)
+            }
+            else
+            {
+                if let urlContent = data
+                {
+                    do
+                    {
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let currencyRate = jsonResult as? NSArray
+                        {
+                            for i in 0..<1 //currencyRate.count ile hepsi alınabilir
+                            {
+                                if let dollarBuying = (currencyRate[i] as? NSDictionary)?["buying"] as? Double
+                                {
+                                    print("Alis:\(dollarBuying)")
+                                    
+                                    DispatchQueue.main.async(execute:{
+                                        self.buyingDollar.text = String(dollarBuying)
+                                    })
+
+                                }
+                                if let dollarSelling = (currencyRate[i] as? NSDictionary)?["selling"] as? Double
+                                {
+                                    print("Satis:\(dollarSelling)")
+                                    DispatchQueue.main.async(execute: {
+                                        self.sellingDollar.text = String(dollarSelling)
+                                    })
+                                }
+                                
+
+                            }
+                        }
+
+
+                    }
+                    
+                    catch
+                    {
+                        print("HATA")
+                    }
+
+                }
+            }
+        }
+        
+        task.resume()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
